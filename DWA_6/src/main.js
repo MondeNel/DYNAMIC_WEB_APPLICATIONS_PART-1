19a1e939dd332c7ac6ec80ab076c3b56a98b320c
Monde Nel <mondenel1996@gmail.com>
2023-08-24 21:32:06 +0200

    added searchAndFilter file that would handle the search for authors and books

--- DWA_6/src/main.js
+++ DWA_6/src/main.js
@@ -3,6 +3,8 @@ import { loadMoreBooks, loadInitialBooks } from './loadMoreBooks.js';
 import { initializeGenres } from './genres.js';
 import { initializeAuthors } from './authors.js';
 import { initializeTheme } from './theme.js';
+import { handleSearchFormSubmit, updateBookList } from './searchAndFilter.js';
+
 
 const booksList = books; // Assuming books array is available
 let matches = books.slice(0, BOOKS_PER_PAGE);
@@ -92,7 +94,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-/** ============================================= data-settings-form =============================================== */
+
+
+/** ========================================== data-settings-form =============================================== */
 
 const settingsForm = document.querySelector('[data-settings-form]');
 
@@ -100,12 +104,18 @@ function handleSettingsFormSubmit(event) {
     event.preventDefault(); // Prevent the default form submission behavior
 }
 
-settingsForm.addEventListener('submit', handleSettingsFormSubmit);
-
 const searchForm = document.querySelector('[data-search-form]');
+if (searchForm) {
+    searchForm.addEventListener('submit', event => {
+        handleSearchFormSubmit(event, booksList, updateBookList);
+    });
+}
+
+settingsForm.addEventListener('submit', handleSettingsFormSubmit);
 
 // Add an event listener to the search form's submit button
 searchForm.addEventListener('submit', handleSearchFormSubmit);
+
 
 
 
@@ -250,105 +260,6 @@ document.querySelector('[data-header-search]').addEventListener('click', functio
 
 
 /** =========================================== data-search-form ============================================= */
-
-function handleSearchFormSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const filters = Object.fromEntries(formData);
-    const results = [];
-
-    for (const book of booksList) {
-        const titleMatch = filters.title.trim() === '' || book.title.toLowerCase().includes(filters.title.toLowerCase());
-        const authorMatch = filters.author === 'any' || book.author === filters.author;
-        let genreMatch = true;
-
-        if (filters.genre !== 'any') {
-            genreMatch = book.genres.includes(filters.genre);
-        }
-
-        if (titleMatch && authorMatch && genreMatch) {
-            results.push(book);
-        }
-    }
-
-    // Call the function to update the book list with the filtered results
-    updateBookList(results);
-}
-
-
-// Implement the function to update the book list
-function updateBookList(results) {
-    // Get references to the required elements
-    const dataListItems = document.querySelector('[data-list-items]');
-    const dataListButton = document.querySelector('[data-list-button]');
-    const dataSearchOverlay = document.querySelector('[data-search-overlay]');
-    const dataListMessage = document.querySelector('[data-list-message]');
-
-    // Calculate the remaining books count to be displayed in the 'Show more' button
-    const remainingBooks = results.length - (page * BOOKS_PER_PAGE);
-    const hasRemaining = remainingBooks > 0;
-    const remaining = hasRemaining ? remainingBooks : 0;
-
-    // Disable the 'Show more' button if there are no remaining books
-    if (dataListButton) {
-        dataListButton.disabled = !hasRemaining;
-    }
-
-    // Set the innerHTML of the 'Show more' button to display the remaining books count
-    if (dataListButton) {
-        dataListButton.innerHTML = `
-            <span>Show more</span>
-            <span class="list__remaining">(${remaining})</span>
-        `;
-    }
-
-    // Create a fragment to hold the new book previews
-    const fragment = document.createDocumentFragment();
-
-    for (const book of results) {
-        const { author: authorId, id, image, title } = book;
-        const element = document.createElement('button');
-        element.classList.add('preview');
-        element.setAttribute('data-preview', id);
-
-        element.innerHTML = `
-            <img class="preview__image" src="${image}" />
-            <div class="preview__info">
-                <h3 class="preview__title">${title}</h3>
-                <div class="preview__author">${authors[authorId]}</div>
-            </div>
-        `;
-
-        fragment.appendChild(element);
-    }
-
-    // Clear existing content in the 'data-list-items' container
-    dataListItems.innerHTML = '';
-
-    // Append the fragment to the 'data-list-items' container
-    dataListItems.appendChild(fragment);
-
-    // Show or hide the "No results found" message based on the presence of search results
-    if (dataListMessage) {
-        dataListMessage.style.display = results.length === 0 ? 'block' : 'none';
-    }
-
-    // Show or hide the overlay buttons based on the presence of search results
-    if (dataListButton && dataListMessage) {
-        dataListButton.style.display = results.length > 0 ? 'block' : 'none';
-        dataListMessage.style.display = results.length === 0 ? 'block' : 'none';
-    }
-
-    // Scroll to the top of the page smoothly
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-
-    // Close the search overlay by setting its 'open' attribute to false
-    if (dataSearchOverlay) {
-        dataSearchOverlay.open = false;
-    }
-}
-
-
 
 
 
