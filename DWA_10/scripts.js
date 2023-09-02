a2a785272f51c670289af41596d6f600b9e922f1
Monde Nel <mondenel1996@gmail.com>
2023-09-02 14:59:02 +0200

    added the reset[alert] message towards the html structure, and applied it to the reset function to display a message when the reset button is being clicked

--- DWA_10/scripts.js
+++ DWA_10/scripts.js
@@ -1,20 +1,20 @@
-
 let COUNTER = 0;
 
 document.querySelector(".incrementButton").addEventListener('click', () => {
     COUNTER += 1;
-    COUNTER = document.querySelector(".counter").textContent = COUNTER;
+    document.querySelector(".counter").textContent = COUNTER;
 });
 
-
 document.querySelector(".decrementButton").addEventListener('click', () => {
-    if (COUNTER <= 0) {
-        COUNTER -= 1;
-        document.querySelector(".counter").textContent = COUNTER;
-    }
+    COUNTER -= 1;
+    document.querySelector(".counter").textContent = COUNTER;
 });
 
 document.querySelector(".resetButton").addEventListener('click', () => {
-    COUNTER = 0;
-    document.querySelector(".counter").textContent = COUNTER;
+    if (COUNTER !== 0) {
+        COUNTER = 0;
+        document.querySelector(".counter").textContent = COUNTER;
+        const resetAlert = document.querySelector(".resetAlert");
+        resetAlert.open = true; // Display reset message
+    }
 });
