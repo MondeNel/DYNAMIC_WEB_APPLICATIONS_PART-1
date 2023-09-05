ee34995dd31fb1190814cd9782c0492489e3b886
Monde Nel <mondenel1996@gmail.com>
2023-09-05 12:59:26 +0200

    added jsdoc for the script file, and modified the height of the progress bar

--- DWA_10/scripts.js
+++ DWA_10/scripts.js
@@ -1,22 +1,44 @@
+
+/**
+ * The current tally count.
+ * @type {number}
+ */
 let COUNTER = 0;
+
+/**
+ * The maximum count allowed.
+ * @type {number}
+ */
 const MAX_COUNT = 10;
 
+/**
+ * Updates the progress bar based on the current tally count.
+ */
 function updateProgressBar() {
     const progressBar = document.querySelector(".progress-bar");
     const percentage = (COUNTER / MAX_COUNT) * 100;
     progressBar.value = percentage;
 }
 
+/**
+ * Displays the reset message.
+ */
 function showResetMessage() {
     const resetAlert = document.querySelector(".resetAlert");
     resetAlert.open = true; // Display reset message
 }
 
+/**
+ * Hides the reset message.
+ */
 function hideResetMessage() {
     const resetAlert = document.querySelector(".resetAlert");
     resetAlert.open = false; // Hide reset message
 }
 
+/**
+ * Increments the tally counter and updates the display.
+ */
 function incrementCounter() {
     if (COUNTER < MAX_COUNT) {
         COUNTER += 1;
@@ -25,6 +47,9 @@ function incrementCounter() {
     }
 }
 
+/**
+ * Decrements the tally counter and updates the display.
+ */
 function decrementCounter() {
     if (COUNTER > 0) {
         COUNTER -= 1;
@@ -33,13 +58,15 @@ function decrementCounter() {
     }
 }
 
+/**
+ * Resets the tally counter to zero and displays a confirmation message.
+ */
 function resetCounter() {
     if (COUNTER !== 0) {
         COUNTER = 0;
         document.querySelector(".counter").textContent = COUNTER;
         updateProgressBar();
         showResetMessage(); // Display reset message when resetting
-        setTimeout(hideResetMessage, 3000); // Hide reset message after 3 seconds (adjust as needed)
     }
 }
 
