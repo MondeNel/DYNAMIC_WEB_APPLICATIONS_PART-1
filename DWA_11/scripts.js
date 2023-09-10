7c9f3f83923b913aa4d6fa52a6ab6507d6a57a45
Monde Nel <mondenel1996@gmail.com>
2023-09-10 09:40:22 +0200

    installed redux, set up the structure of the file

---
+++ DWA_11/scripts.js
@@ -0,0 +1,37 @@
+// Action creators
+const increment = () => {
+    return {
+        type: 'INCREMENT'
+    }
+}
+
+const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    }
+}
+
+const reset = () => {
+    return {
+        type: 'RESET'
+    }
+}
+
+// Reducer
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        case 'RESET':
+            return 0;
+        default:
+            return state;
+    }
+};
+
+// Create the Redux store
+const store = Redux.createStore(counter);
+
+
