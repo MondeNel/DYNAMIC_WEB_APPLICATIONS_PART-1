51052105ba19f41c92ea536555d6915942ca9a32
Monde Nel <mondenel1996@gmail.com>
2023-09-11 18:00:19 +0200

    re-submit the challenge, because I misunderstood the difference between a factory function and a normal function, what was expected from me was to make use of a factory function which returns an Object

--- DWA_8/CODE/previewFunctionality.js
+++ DWA_8/CODE/previewFunctionality.js
@@ -1,56 +1,30 @@
-//@ts-check
-
 /**
- * Creates a preview element for a book.
+ * Creates a book preview object.
  *
  * @param {Object} bookData - The data of the book for which to create the preview.
  * @param {Object} authors - An object containing author information with author IDs as keys and author names as values.
- * @returns {HTMLElement} The created preview element.
+ * @returns {Object} The created book preview object.
  */
 export function createPreview(bookData, authors) {
     // Destructure bookData
     const { author, id, image, title, genre } = bookData;
 
-    // Create the main preview element
-    const preview = document.createElement('div');
-    preview.classList.add('preview');
-    preview.setAttribute('data-preview', id);
+    // Create the book preview object
+    const bookPreview = {
+        id: id,
+        image: image,
+        title: title,
+        genre: genre,
+        author: authors[author],
+        /**
+         * Get a description of the book preview.
+         *
+         * @returns {string} The description of the book preview.
+         */
+        getDescription() {
+            return `This is a book preview with title "${this.title}" by ${this.author}, in the genre of ${this.genre}, and an image URL of "${this.image}"`;
+        },
+    };
 
-    // Create the book image element
-    const imageElement = document.createElement('img');
-    imageElement.classList.add('preview__image');
-    imageElement.src = image;
-
-    // Create the book information container
-    const info = document.createElement('div');
-    info.classList.add('preview__info');
-
-    // Create the book title element
-    const titleElement = document.createElement('h3');
-    titleElement.classList.add('preview__title');
-    titleElement.textContent = title;
-
-    // Create the book genre element
-    const genreElement = document.createElement('div');
-    genreElement.classList.add('preview__genre');
-    genreElement.textContent = genre;
-
-    // Get the author's name from the authors object
-    const authorName = authors[author];
-
-    // Create the book author element
-    const authorElement = document.createElement('div');
-    authorElement.classList.add('preview__author');
-    authorElement.textContent = authorName;
-
-    // Append elements to the info container
-    info.appendChild(genreElement);
-    info.appendChild(titleElement);
-    info.appendChild(authorElement);
-
-    // Append elements to the main preview element
-    preview.appendChild(imageElement);
-    preview.appendChild(info);
-
-    return preview;
+    return bookPreview;
 }
